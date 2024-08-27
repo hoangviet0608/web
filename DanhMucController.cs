51ffd1130398c2f1d525cd2458b69177ddccee3c
Nguyen Hoang Viet <136926143+hoangviet0608@users.noreply.github.com>
2024-08-27 20:19:16 +0700

    Add files via upload

---
+++ DanhMucController.cs
@@ -0,0 +1,27 @@
+﻿using QLBH.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLBH.Controllers
+{
+    public class DanhMucController : Controller
+    {
+        DataClasses1DataContext db = new DataClasses1DataContext("Data Source=DESKTOP-0746MOR;Initial Catalog=qlbh1;Integrated Security=True;TrustServerCertificate=True");
+
+        // GET: DanhMuc
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetCategories()
+        {
+            var ketqua = "goi thanh cong";
+            return Json(ketqua, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
